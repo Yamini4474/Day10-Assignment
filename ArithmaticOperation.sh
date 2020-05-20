ba15c24ca04790c3f9f4f9b3ef386cb6dedc8476
Yamini <Yamini4474@gmail.com>
2020-05-20 20:12:24 +0530

    code updated with dictionaries

--- ArithmaticOperation.sh
+++ ArithmaticOperation.sh
@@ -1,14 +1,17 @@
 #!/bin/bash -x
-
+declare -A results
 read -p "Take First Input " a
 read -p "Take Second Input " b
 read -p "Take Third Input " c
-d=$(($a+$b*$c))
-e=$(($a*$b+$c))
-f=$(($c+$a/$b))
-g=$(($a%$b+$c))
-echo $d;
-echo $e;
-echo $f;
-echo $g;
+result[d]=$(($a+$b*$c))
+result[e]=$(($a*$b+$c))
+result[f]=$(($c+$a/$b))
+result[g]=$(($a%$b+$c))
+echo "$d result " ${result[d]}
+echo "$e result " ${result[e]}
+echo "$f result " ${result[f]}
+echo "$g result " ${result[g]}
+echo "All Values Result " ${results[@]}
+echo "Values " ${!results[@]}
+echo "Number of Values " ${#results[@]}
 
