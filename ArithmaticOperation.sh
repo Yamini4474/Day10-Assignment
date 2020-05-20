897279a352812fbc252109940d7ea045afef5868
Yamini <Yamini4474@gmail.com>
2020-05-20 19:55:47 +0530

    code updated

--- ArithmaticOperation.sh
+++ ArithmaticOperation.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -x
-declare -A result
+
 read -p "Take First Input " a
 read -p "Take Second Input " b
 read -p "Take Third Input " c
@@ -7,7 +7,8 @@ d=$(($a+$b*$c))
 e=$(($a*$b+$c))
 f=$(($c+$a/$b))
 g=$(($a%$b+$c))
-result[a+b*c]="$d"
-result[a*b+c]="$e"
-result[c+a/b]="$f"
-result[a%b+c]="$g"
+echo $d;
+echo $e;
+echo $f;
+echo $g;
+
