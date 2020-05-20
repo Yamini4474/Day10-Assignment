45daa7065c898bb54221057db3097da41a1d6c47
Yamini <Yamini4474@gmail.com>
2020-05-20 19:54:04 +0530

    code updated

--- ArithmaticOperation.sh
+++ ArithmaticOperation.sh
@@ -1,11 +1,9 @@
-<<<<<<< HEAD
-f=$(($c+$a/$b))
-=======
 #!/bin/bash -x
-declare -A result
+
 read -p "Take First Input " a
 read -p "Take Second Input " b
 read -p "Take Third Input " c
 d=$(($a+$b*$c))
 e=$(($a*$b+$c))
->>>>>>> UC3
+f=$(($c+$a/$b)
+
